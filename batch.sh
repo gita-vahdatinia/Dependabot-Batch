2c68e1e74b1c222a00c12e51a8662a3a0b459d77
Gita Vahdatinia <gita@coda.io>
2022-07-26 11:16:19 -0700

    add create PR

--- batch.sh
+++ batch.sh
@@ -22,26 +22,26 @@ PATCHED=()
 for br in $(echo "${LIST_OF_BRANCHES}" | sort); do
   # diff package.json and Pipfile from the remote dependabot branch and then apply as a local patch.
   echo "${txtbld}Patching: $br ${txtdef}"
-  git diff "origin/${br}~...origin/${br}" -- ${PACKAGE_FILES} | git apply -C0 && PATCHED+=(${br}) || echo "${bldred}WARNING: Skipping failed patch ${br}${txtdef}"
+  PATCHED+=(${br})
 done
 
-cat package.json
-# if [[ -z "${PATCHED[*]}" ]]; then
-#   echo "${bldred}ERROR: all patches failed ¯\_(ツ)_/¯"
-#   exit 1
-# fi
+echo ${PATCHED}
+if [[ -z "${PATCHED[*]}" ]]; then
+  echo "${bldred}ERROR: all patches failed ¯\_(ツ)_/¯"
+  exit 1
+fi
 
 # Finalize tweaks to yarn.lock file by installing from package.json
-# if [[ "${PATCHED[*]}" == *"/npm_and_yarn/"* ]]; then
-echo "${txtbld}Finalizing yarn.lock updates${txtdef}"
-yarn install
-# fi
+if [[ "${PATCHED[*]}" == *"/npm_and_yarn/"* ]]; then
+  echo "${txtbld}Finalizing yarn.lock updates${txtdef}"
+  yarn install
+fi
 
 # Finalize Pipfile.lock file by installing from Pipfile
-# if [[ "${PATCHED[*]}" == *"/pip/"* ]]; then
-echo "${txtbld}Finalizing Pipfile.lock updates${txtdef}"
-pipenv install
-# fi
+if [[ "${PATCHED[*]}" == *"/pip/"* ]]; then
+  echo "${txtbld}Finalizing Pipfile.lock updates${txtdef}"
+  pipenv install
+fi
 
 echo "${txtbld}Successfully patched: ${PATCHED[*]}${txtdef}"
 git checkout -b ${BRANCH_TITLE}
@@ -58,4 +58,11 @@ $(
 )
 ------------------------------------------------------------------------------
 " -- ${PACKAGE_FILES} ${LOCK_FILES}
- git push --set-upstream origin ${BRANCH_TITLE}
+PR_TITLE=$(echo "${BRANCH_TITLE} | tr "-" " " | tr "_" " " | tr "/" " ")
+git push --set-upstream origin ${BRANCH_TITLE}
+curl \
+-X POST \
+-H "Accept: application/vnd.github+json" \ 
+-H "Authorization: token ${GITHUB_TOKEN}" \
+https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls \
+-d '{"title":"${PR_TITLE}","head":"github-actions:${BRANCH_TITLE}","base":"main"}'
